15e85f531fd45fa8447a03d949a8ca8497f826cb
WonJin <try3982@kakao.com>
2025-02-04 20:17:18 +0900

    jparepository

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -1,5 +1,9 @@
 package zerobase.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "memo") // memo 테이블에서 가져올것이다!
 public class Memo {
+    @Id // primary key는 id야
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //기본적인 키 생성을 db에 맡기겠다.
     private int id;
     private String text;
 
